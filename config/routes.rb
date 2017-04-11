ea111f55298d0a6732a290c2beb1918cdefa9650
triggerdog <tiff0402@hotmail.com>
2017-04-11 07:02:43 +0000

    added contact form view, route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root to: 'pages#home'
     get 'about', to: 'pages#about'
+    resources  :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
