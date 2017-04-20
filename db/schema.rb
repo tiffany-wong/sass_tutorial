643049a4f85c92d1af2388657010d06a9eb5778c
triggerdog <tiff0402@hotmail.com>
2017-04-20 06:03:39 +0000

    created contacts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417074619) do
+ActiveRecord::Schema.define(version: 20170420060059) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
